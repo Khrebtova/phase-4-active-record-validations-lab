2a56fbc9ce2f1084636514fb420d0ad2a1a1ef1a
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-19 14:00:55 -0700

    commit tree d60b55687ee6a868fea4bd5150dfecf9080a6ee4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+   
+    validates :title, presence: true 
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait?
+
+    
+    WORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+    def title_is_clickbait?        
+        if WORDS.none?{|el| el.match title}
+            errors.add(:title, "must include 'Won't Believe', 'Secret', 'Top', or 'Guess'")
+        end
+    end
+    # def title_is_clickbait?        
+    #     result = title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+    #     if !result 
+    #         errors.add(:title, "must include 'Won't Believe', 'Secret', 'Top', or 'Guess'")
+    #     end
+    # end
 end
